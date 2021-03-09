63d444546b3e3f0d166d70c454782b023ddf8e0a
KristinaMarinova <kriska2000@abv.bg>
2021-03-09 15:23:05 +0200

    solve second task

---
+++ Entity Framework Core/01.DB APPS INTRODUCTION/Exercise/FetchingResultsetsWithAdoNet/02.Villain Names/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using _01.InitialSetup;
+using System;
+using System.Data.SqlClient;
+
+namespace _02.Villain_Names
+{
+    class StartUp : ConnectionOptions
+    {
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            using (connection)
+            {
+                var queryText = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                  FROM Villains AS v 
+                                  JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                  GROUP BY v.Id, v.Name 
+                                  HAVING COUNT(mv.VillainId) > 3 
+                                  ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand cmd = new SqlCommand(queryText, connection);
+
+                var reader = cmd.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                    }
+                }
+
+            }
+        }
+    }
+}

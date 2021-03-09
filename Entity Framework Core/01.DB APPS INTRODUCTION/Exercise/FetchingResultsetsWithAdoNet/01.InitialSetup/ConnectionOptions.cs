57b4df262db8b2db1d3bd5edb3a857c34e37ee31
KristinaMarinova <kriska2000@abv.bg>
2021-03-09 15:22:55 +0200

    solve first task

---
+++ Entity Framework Core/01.DB APPS INTRODUCTION/Exercise/FetchingResultsetsWithAdoNet/01.InitialSetup/ConnectionOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _01.InitialSetup
+{
+    public class ConnectionOptions
+    {
+        public static string connectionString = "Server=.;Database=MinionsDB;Integrated Security=true";
+        public static SqlConnection connection = new SqlConnection(String.Format(connectionString, "master"));
+    }
+}
